5dc99424f1b22fa934acd9121ed7947b53d56dcb
rifath33 <hrifath20@stuy.edu>
2021-01-05 06:36:53 -0500

    made MakeStars

---
+++ MakeStars.java
@@ -0,0 +1,17 @@
+/* 
+Write a java program MakeStars.java that will read in text input, and
+convert each word to an equally long block of *. The converted text is
+then printed. New Lines will be preserved. 
+
+We will combine multiple whitespaces between words into spaces. 
+*/
+
+import java.util.Scanner;
+public class MakeStars {
+  public static void main(String args[]) {
+
+  }
+
+}
+
+
