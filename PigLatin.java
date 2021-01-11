05778994839251aa31275999d093abe8bd1297f5
rifath33 <hrifath20@stuy.edu>
2021-01-11 01:53:55 -0500

    got a working pigLatinSimple method, testing using konstantinovich sample and the letter f

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,39 @@
 public class PigLatin{
   public static void main(String[] args){
     
+    System.out.println(pigLatinSimple("f"));
+    
+    System.out.println(pigLatinSimple("mock"));
+    System.out.println(pigLatinSimple("pie"));
+    System.out.println(pigLatinSimple("david"));
+    System.out.println(pigLatinSimple("aaron"));
+    
   }
+  
+  public static String pigLatinSimple(String s){
+    String result = "";
+    
+    if(
+    
+    s.charAt(0) == ('a') ||
+    s.charAt(0) == ('e') ||
+    s.charAt(0) == ('i') ||
+    s.charAt(0) == ('o') ||
+    s.charAt(0) == ('u')
+    
+    )
+    result = s + "hay";
+    else{
+        
+        result = s.substring(1) + s.charAt(0);
+        result = result + "ay";
+        
+    }
+    
+    return result;
+    
+  }
+  
+  
+  
 }
